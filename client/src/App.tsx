e900d92805a45a1ba558f0ce0e950696c3c03b60
sungu.lee <sungu.lee@huconn.com>
2020-06-22 03:39:15 +0900

    ssss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -167,6 +167,8 @@ const App: React.FC = () => {
       <article className="App-article">
         <DeviceList deviceInfo={deviceInfo} />
         <hr />
+        {values.length}
+        {deviceInfo.length}
         <h1>Overall Voltage</h1>
         <div className="App-graph-grid">        
           {values.length === 0 && deviceInfo.length === 0 && <h1 className="noData">No data available</h1>}
