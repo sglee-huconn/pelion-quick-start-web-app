a77a7264759749df2cece6ac4a8e34809ead02f5
sungu.lee <sungu.lee@huconn.com>
2020-06-24 20:17:12 +0900

    test

--- client/src/resourceBmsInfoEtc.tsx
+++ client/src/resourceBmsInfoEtc.tsx
@@ -1,66 +1,142 @@
 import moment, { Duration } from "moment";
 import React from "react";
-import { DeviceInfo } from ".";
+import { Devices, Names, Paths, ResourceValue } from ".";
 
 moment.defaultFormat = "YYYY-MM-DD HH:mm:ss";
 
-interface DeviceListProps {
-  deviceInfo: DeviceInfo[];
+interface ToolbarProps {
+  devices: Devices;
+  resourceNames: Names;
+  deviceNames: Names;
 }
 
-const ResourceBmsInfoEtc: React.FunctionComponent<DeviceListProps> = ({ deviceInfo }) => {
-  if (deviceInfo.length === 0) {
-    return null;
-  }
-  const zeroPad = (num: string, size = 2) => ("000000000" + num).substr(-size);
+const ResourceBmsInfoEtc: React.FunctionComponent<ToolbarProps> = ({ devices, deviceNames, resourceNames }) => {
 
-  const formatDuration = (d: Duration) => {
-    const duration = [d.years(), d.months(), d.days(), d.hours(), d.minutes(), d.seconds()];
-    return duration.map(e => zeroPad(e.toString())).join(" ");
-  };
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>First value seen</th>
-          <th>Latest value seen</th>
-          <th>Connection Up time</th>
-          <th>Counter value</th>
-          <th>Up time based on counter</th>
-          <th>Online</th>
-        </tr>
-      </thead>
-      <tbody>
-        {deviceInfo.map(d => {
-          const last = moment(d.latest_update);
-          const first = moment(d.first_update);
-          const uptime = moment.duration(last.diff(first));
-          const stillUp = moment.duration(moment().diff(last), "milliseconds");
-          const appUpTime = moment.duration(d.latest_value * 5, "seconds");
-          return (
-            <tr key={d.id}>
-              <td title={d.device_id}>{d.name}</td>
-              <td title={first.fromNow()}>
-                <code>{first.format()}</code>
-              </td>
-              <td title={last.fromNow()}>
-                <code>{last.format()}</code>
-              </td>
-              <td title={uptime.humanize()}>
-                <code>{formatDuration(uptime)}</code>
-              </td>
-              <td>{d.latest_value}</td>
-              <td title={appUpTime.humanize()}>
-                <code>{formatDuration(appUpTime)}</code>
-              </td>
-              <td>{stillUp.asMinutes() >= 5 ? "?Ÿ‘?" : "?œ…"}</td>
+  const showDevice(paths: Paths, deviceId: string, resNames: Names) => {
+    const remainCapacityPath = Object.keys(resNames)[0];
+    const battPhysicalCapacityPath = Object.keys(resNames)[1];
+    const batteryRemainCapacityPath = Object.keys(resNames)[2];
+    const batteryCycleCapacityPath = Object.keys(resNames)[3];
+    const currentPowerPath = Object.keys(resNames)[4];
+
+    const cellAverageVoltagePath = Object.keys(resNames)[5];
+    const cellLowestVoltagePath = Object.keys(resNames)[6];
+    const cellHighestVoltagePath = Object.keys(resNames)[7];
+    const cellNumberLowestVoltagePath = Object.keys(resNames)[8];
+    const cellNumberHighestVoltagePath = Object.keys(resNames)[9];
+
+    let remainCapacity: string | number = 'no data';
+    let battPhysicalCapacity: string | number = 'no data';
+    let batteryRemainCapacity: string | number = 'no data';
+    let batteryCycleCapacity: string | number = 'no data';
+    let currentPower: string | number = 'no data';
+
+    let cellAverageVoltage: string | number = 'no data';
+    let cellLowestVoltage: string | number = 'no data';
+    let cellHighestVoltage: string | number = 'no data';
+    let cellNumberLowestVoltage: string | number = 'no data';
+    let cellNumberHighestVoltage: string | number = 'no data';    
+
+    if (paths[remainCapacityPath] !== undefined) {
+      const [val1, val2] = paths[remainCapacityPath];
+      remainCapacity = val1.value;     
+    }
+
+    if (paths[battPhysicalCapacityPath] !== undefined) {
+      const [val1, val2] = paths[battPhysicalCapacityPath];
+      battPhysicalCapacity = val1.value;     
+    }  
+
+    if (paths[batteryRemainCapacityPath] !== undefined) {
+      const [val1, val2] = paths[batteryRemainCapacityPath];
+      batteryRemainCapacity = val1.value;     
+    }      
+
+    if (paths[batteryCycleCapacityPath] !== undefined) {
+      const [val1, val2] = paths[batteryCycleCapacityPath];
+      batteryCycleCapacity = val1.value;     
+    }         
+    
+    if (paths[currentPowerPath] !== undefined) {
+      const [val1, val2] = paths[currentPowerPath];
+      currentPower = val1.value;     
+    }          
+
+
+    if (paths[cellAverageVoltagePath] !== undefined) {
+      const [val1, val2] = paths[cellAverageVoltagePath];
+      cellAverageVoltage = val1.value;     
+    }
+
+    if (paths[cellLowestVoltagePath] !== undefined) {
+      const [val1, val2] = paths[cellLowestVoltagePath];
+      cellLowestVoltage = val1.value;     
+    }  
+
+    if (paths[cellHighestVoltagePath] !== undefined) {
+      const [val1, val2] = paths[cellHighestVoltagePath];
+      cellHighestVoltage = val1.value;     
+    }      
+
+    if (paths[cellNumberLowestVoltagePath] !== undefined) {
+      const [val1, val2] = paths[cellNumberLowestVoltagePath];
+      cellNumberLowestVoltage = val1.value;     
+    }         
+    
+    if (paths[cellNumberHighestVoltagePath] !== undefined) {
+      const [val1, val2] = paths[cellNumberHighestVoltagePath];
+      cellNumberHighestVoltage = val1.value;     
+    }              
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>remain capacity(%)</th>
+            <th>battery physical capacity(Ah)</th>
+            <th>battery remain capacity(Ah)</th>
+            <th>battery cycle capacity(Ah)</th>
+            <th>current power(W)</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td title={remainCapacity}></td>
+              <td title={battPhysicalCapacity}></td>
+              <td title={batteryRemainCapacity}></td>
+              <td title={batteryCycleCapacity}></td>
+              <td title={currentPower}></td>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>cell average voltage(V)</th>
+            <th>cell lowest voltage(V)</th>
+            <th>cell highest voltage(V)</th>
+            <th>cell number lowest voltage(Ah)</th>
+            <th>cell number highest voltage(W)</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td title={cellAverageVoltage}></td>
+              <td title={cellLowestVoltage}></td>
+              <td title={cellHighestVoltage}></td>
+              <td title={cellNumberLowestVoltage}></td>
+              <td title={cellNumberHighestVoltage}></td>
+            </tr>
+        </tbody>
+      </table>      
+    );
+  }
+
+  const showDevices = (d: Devices, r: Names) => 
+    Object.keys(d)
+    .map(res => showDevice(d[res], res, r));  
+
+  return <React.Fragment>{showDevices(devices, resourceNames)}</React.Fragment>
 };
 
 export default ResourceBmsInfoEtc;
